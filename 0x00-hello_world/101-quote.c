870c8a3244daa98f5a7a197369c8f90c958fc6a6
RaniaBen00 <raniabenali9@gmail.com>
2023-06-18 14:05:50 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - A C program that prints a line to the standard error
  *
- *Return: Always 1 (Success)
+ *Return: Always 1.
  */
 int main(void)
 {
-puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+puts(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
