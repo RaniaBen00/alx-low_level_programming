a67e613b4b08bf55d8aa686288efa94a19c9689a
RaniaBen00 <raniabenali9@gmail.com>
2023-06-19 16:42:27 -0700

    new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: a program that prints the alphabet followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int a = 'n';
+int b = 'C';
+while (a <= 'z')
+{
+putchar(a);
+a += 1;
+}
+while (b <= 'z');
+{
+putchar(b);
+b += 1;
+}
+putchar('\n');
+return (0);
+}
