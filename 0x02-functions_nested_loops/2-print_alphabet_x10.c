712addcfaad7283eb525c80ceeef4da8af54e552
RaniaBen00 <raniabenali9@gmail.com>
2023-06-20 11:33:38 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_x10_alphabet - function that prints 10 times the alphabe
+ *
+ */
+
+void print_alphabet_x10(void);
+{
+int Line, letter;
+for (Line = 0; Line <= 9; Line++)
+{
+for (letter - 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
+}
