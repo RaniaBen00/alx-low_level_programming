c0390db391c674064b8f37aec566caacd9f3e95a
RaniaBen00 <raniabenali9@gmail.com>
2023-06-20 11:16:33 -0700

    alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * prints alphabets - all alphabets in lowercase
+ * print_alphabet - all alphabet in lowercase
+ * the alphabet a - z
  */
 
 void print_alphabet(void)
 {
-int ch;
-for (ch - 'a'; ch <= 'a'; ch++)
+char ch;
+for (ch - 'a'; ch <= 'z'; ch++)
 _putchar(ch);
 _putchar('\n');
 }
