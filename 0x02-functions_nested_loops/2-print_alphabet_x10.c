97d5bc18f0893f7357ca3572e905e3b7e564c2e9
RaniaBen00 <raniabenali9@gmail.com>
2023-06-20 11:55:24 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 int Line, letter;
 for (Line = 0; Line <= 9; Line++)
 {
-for (letter - 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('\n');
 }
