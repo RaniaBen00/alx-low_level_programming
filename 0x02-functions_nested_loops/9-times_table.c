8e4d1856ba5fd2620b25600a1cb58c84d00fb2d5
RaniaBen00 <raniabenali9@gmail.com>
2023-06-20 17:25:05 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,14 +18,14 @@ _putchar (k + '0');
 }
 if (k < 10 && n != 0)
 {
-_putchar((',');
+_putchar(',');
 _putchar(' ');
 _putchar(' ');
-_putchar(k + 0);
+_putchar(k + '0');
 }
 else if (k >= 10)
 {
-_putchar((',');
+_putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
