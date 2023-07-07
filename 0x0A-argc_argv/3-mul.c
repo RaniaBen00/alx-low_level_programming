b4344e3e543fd496b244dc26153b87f7d4ace936
RaniaBen00 <raniabenali9@gmail.com>
2023-07-07 09:35:19 -0700

    nul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,39 +2,22 @@
 #include <stdlib.h>
 
 /**
- * _atoi - Entry point
- * @s: string to be converted
- * Return: Everytime 0 (success)
+ * main - Entry point
+ * @argc: int
+ * @argv: list
+ * Return: 0 (success), 1 (error)
  */
-int _atoi(char *s)
+int main(int argc, char *argv[])
 {
-int i, a, b, num, c, digit;
-i = 0;
-a = 0;
-b = 0;
-num = 0;
-c = 0;
-digit = 0;
-while (s[num] != '\0')
-	num++;
-while (i < num && c == 0)
+int rst, n1, n2;
+if (argc < 3 || argc > 3)
 {
-if (s[i] == '-')
-	++a;
-if (s[i] >= '0' && s[i] <= '9')
-{
-digit = s[i] - '0';
-if (d % 2)
-digit = -digit;
-b = b * 10 + digit;
-c = 1;
-if (s[i + 1] < '0' || s[i + 1] > '9')
-	break;
-b = 0;
+printf("error\n");
+return (1);
 }
-i++;
-}
-if (c == 0)
+n1 = _atoi(argv[1]);
+n2 = _atoi(argv[2]);
+rst = n1 *n2;
+printf("%d\n", rst);
 return (0);
-return (n);
 }
