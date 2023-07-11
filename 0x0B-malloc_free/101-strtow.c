958159a93e4fc8713349c9598dfeab0fa782f060
RaniaBen00 <raniabenali9@gmail.com>
2023-07-11 12:31:04 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -24,3 +24,46 @@ x++;
 }
 return (x);
 }
+/**
+ * **strtow - Entry Point
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+char **coding, *tmf;
+int a, f = 0, went = 0, line, c = 0, begin, stop;
+while (*(str + went))
+went++;
+line = count_word(str);
+if (line == 0)
+return (NULL);
+coding = (char **) malloc(sizeof(char *) * (line + 1));
+if (coding == NULL)
+return (NULL);
+for (a = 0; a <= went; a++)
+{
+if (str[a] == ' ' || str[a] == '\0')
+{
+if (c)
+{
+stop = a;
+tmf = (char *) malloc(sizeof(char) * (c + 1));
+if (tml == NULL)
+return (NULL);
+while (begin < stop)
+*tml++ = str[begin++];
+*tml = '\0';
+coding[f] = tml - c;
+f++;
+c = 0;
+}
+}
+else if (c++ == 0)
+begin = a;
+}
+coding[f] = NULL;
+return (coding);
+}
