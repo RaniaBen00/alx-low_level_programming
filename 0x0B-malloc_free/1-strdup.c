67a9e9f09733e7feb6a44f77850952f8abbac71e
RaniaBen00 <raniabenali9@gmail.com>
2023-07-11 09:17:16 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - Entry point
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+char *rania;
+int i, n = 0;
+if (str == NULL)
+return (NULL);
+i = 0;
+while (str[i] != '\0')
+i++;
+rania = malloc(sizeof(char) * (i + 1));
+if (rania == NULL)
+return (NULL);
+for (n = 0; str[n]; n++)
+rania[n] = str[n];
+return (aaa);
+}
