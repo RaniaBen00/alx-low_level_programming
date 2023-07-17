9f1279368f251d1e1adb4f7cefc58d934e389ee5
RaniaBen00 <raniabenali9@gmail.com>
2023-07-17 15:10:41 -0700

    file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Write a program that prints the name of the file it was compiled from, followed by a new line.
+ * main - Entry Point
  *
  * Return: Everytime 0 (Success)
  */
